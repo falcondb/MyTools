08ede267c5c6e02c98f1e6f5cdcce54bba900f7e
falcondb <linfeng8003@gmail.com>
2021-01-05 11:01:44 -0800

    QEMU script

--- shell/linux-kernel-development.sh
+++ shell/linux-kernel-development.sh
@@ -2,8 +2,8 @@ if [ -z $OSDIST ] || [ -z $INSTALLER ] || [ -z $SUDO ] ; then
 [[ -f $TOOLCOMSH ]] && . $TOOLCOMSH && echo "Loaded the tool script, $TOOLCOMSH"
 fi
 
-OSARCH=x86_64
-QEMUBIN=qemu-system-x86_64
+OSARCH=$(uname -m)
+QEMUBIN=qemu-system-$OSARCH
 
 TOOLS=/home/work/github.com
 
@@ -44,11 +44,24 @@ function build-root-fs {
   popd
 }
 
+# The QEMU options used:
+#   -kernel   Uses bzImage as kernel image. The kernel can be either a Linux kernel or in multiboot format.
+#   -boot     Specifies boot order drives as a string of drive letters.
+#               The x86 PC uses: a, b (floppy 1 and 2), c (first hard disk), d (first CD-ROM), n-p (Etherboot from network adapter 1-4), hard disk boot is the default
+#   -m        Sets guest startup RAM size to megs megabytes. Default is 128 MiB. Optionally, a suffix of “M” or “G” can be used.
+#   -hda      Uses file as hard disk 0, 1, 2 or 3 image
+#   -append   Gives the kernel command line arguments
+#   -serial   Redirects the virtual serial port to host character device dev. The default device is vc in graphical mode and stdio in non graphical mode.
+#   -display  Selects type of display to use. Type none: Do not display video output.  -nographic disable graphical output so that QEMU is a simple command line application.
+#   -enable-kvm Enable KVM full virtualization support.
+#   -no-reboot  Exit instead of rebooting.
+#   -cpu host Select CPU model  VM processor with all supported host features
+#
 function run-qemu {
   [[ -z $1 ]] && KIMG=$LSRC/arch/x86/boot/bzImage || KIMG=$1
-  [[ -z $2 ]] && RTPATH=$LSRC/arch/x86/boot/bzImage || RTPATH=$2
+  [[ -z $2 ]] && RTPATH=$RTFS/output/images/rootfs.ext2 || RTPATH=$2
   $QEMUBIN -kernel $KIMG \
     -boot c -m 2049M -hda $RTPATH \
     -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
-    -serial stdio -display none
+    -serial stdio -display none -enable-kvm -no-reboot
 }
